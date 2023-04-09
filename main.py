2c215f3b7f7da9b9a9b5f6069293b618107b08ce
01110010pl <01110010pl@gmail.com>
2023-04-09 21:53:55 +0200

    Rebuilding of the whole programme

--- main.py
+++ main.py
@@ -1,9 +1,84 @@
 import sys
-from PyQt5 import QtWidgets
-from tictactoe.ticatactoe import MainWindow
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QLabel
+from PySide6.QtCore import QRegularExpression
+import random
+
+class Program:
+    window = 0
+
+    @staticmethod
+    def setDefaultFields():
+        loadedPixMap = QPixmap("content/blank_template.png")
+        boardList = Program.window.findChildren(QLabel, QRegularExpression("board*"))
+        for i in boardList:
+            i.setPixmap(loadedPixMap)
+        Program.window.currentTurn.setPixmap(loadedPixMap)
+
+class Game():
+
+    def setDefault():
+        Game.players = ["x", "o"]
+        Game.board = [['blank', 'blank', 'blank'] for _ in range(3)]
+        Game.currentRound = random.choice(Game.players)
+        Game.counterAvailableFields = 9
+
+    @staticmethod
+    def resetGame():
+        Game.setDefault()
+
+    @staticmethod
+    def nextTurn():
+        if Game.currentRound == Game.players[0]:
+            Game.currentRound = Game.players[1]
+        else:
+            Game.currentRound = Game.players[0]
+        Game.counterAvailableFields -= 1
+
+    @staticmethod
+    def changeField(self, row, column):
+        row -= 1
+        column -= 1
+        if Game.board[row][column] == 'blank':
+            Game.board[row][column] = Game.currentRound
+            return True
+        return False
+    
+    @staticmethod
+    def checkWin():
+        for i in range(3):
+            temporary1 = ''.join(Game.board[i])
+            if  temporary1 == 'xxx' or temporary1 == 'ooo':
+                return True
+            temporary1 = ''.join([Game.board[x][i] for x in range(3)])
+            if  temporary1 == 'xxx' or temporary1 == 'ooo':
+                return True
+        temporary2 = ''.join([Game.board[a][b] for (a, b) in [(0, 0), (1, 1), (2, 2)]])
+        if  temporary2 == 'xxx' or temporary2 == 'ooo':
+            return True
+        temporary2 = ''.join([Game.board[a][b] for (a, b) in [(0, 2), (1, 1), (2, 0)]])
+        if  temporary2 == 'xxx' or temporary2 == 'ooo':
+            return True
+        return False
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    app = QApplication(sys.argv)
+    ui_file_name = "content/mainWindow.ui"
+    ui_file = QFile(ui_file_name)
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+    loader = QUiLoader()
+    Program.window = loader.load(ui_file)
+    ui_file.close()
+    if not Program.window:
+        print(loader.errorString())
+        sys.exit(-1)
+
+    Program.setDefaultFields()
+    Program.window.show()
+
     sys.exit(app.exec())
--- tictactoe/build.sh
+++
@@ -1,4 +0,0 @@
-pyuic5 -x mainWindow.ui -o mainWindow.py
-pyuic5 -x winMessage.ui -o winMessage.py
-pyuic5 -x drawMessage.ui -o drawMessage.py
-pyuic5 -x aboutMessage.ui -o aboutMessage.py
